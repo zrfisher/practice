fe2fa62478f63badb5d2d758612621222234329b
Zoe Fisher <zrfisher@usc.edu>
2019-01-15 14:38:28 -0800

    towers of hanoi with 4

---
+++ hanoi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solveTowers(int disc, char src, char dst, char alt);
+
+int main()
+{
+  solveTowers(3,'a','b','c');
+  return 0;
+}
+
+void solveTowers(int disc, char src, char dst, char alt)
+{
+
+
+
+
+}
