b3d6480f72876efe417479e7915058be79f36780
Zoe Fisher <zrfisher@usc.edu>
2019-01-15 14:41:46 -0800

    towers of hanoi with comments explaining

--- hanoi.cpp
+++ hanoi.cpp
@@ -5,14 +5,21 @@ void solveTowers(int disc, char src, char dst, char alt);
 
 int main()
 {
-  solveTowers(3,'a','b','c');
+  solveTowers(4,'a','b','c');
   return 0;
+}
+
+void move(int disc, char src, char dst){
+	cout << "Move disc from " << src << " to " << dst << endl;
 }
 
 void solveTowers(int disc, char src, char dst, char alt)
 {
-
-
-
-
+	if(disc == 1){
+		move(disc, src, dst);
+		return;
+	}
+	solveTowers(disc-1, src, alt, dst); //put them all except the biggest one on alternate peg
+	move(disc, src, dst); //move the biggest peg to the bottom of the dst peg
+	solveTowers(disc-1, alt, dst, src); //puts the rest of the pegs in order on top of the biggest peg on dst
 }
